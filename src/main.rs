19d775bb39898338c6ecc31b1ad390fcb9f23ab0
Will Speak <lithiumflame@gmail.com>
2021-02-26 22:50:04 +0000

    Refactorings
    Move portal code into its own module and restructure things to be a bit
    more isolated and ergonomic.
    
    Still needs some work to deal with:
    
     * closing of session
     * Observing session errors?

--- src/main.rs
+++ src/main.rs
@@ -1,207 +1,22 @@
-use dbus::{
-    arg::{PropMap, RefArg, Variant},
-    blocking::{Connection, Proxy},
-    Message, Path,
-};
 use pipewire::properties;
-use std::{collections::HashMap, convert, error::Error, ffi::CString, ptr, time::Duration};
+use portal::ScreenCast;
+use std::{error::Error, ffi::CString, ptr};
 
-mod generated;
-use generated::{OrgFreedesktopPortalRequestResponse, OrgFreedesktopPortalScreenCast};
-
-/// D-Bus connection state. Used to access the Desktop portal
-/// and open our screencast.
-struct ConnectionState {
-    connection: Connection,
-    sender_token: String,
-}
-
-impl ConnectionState {
-    /// Open a new D-Bus connection to use for all our requests
-    pub fn open_new() -> Result<Self, dbus::Error> {
-        // Create a new session and work out our session's sender token. Portal
-        // requests will send responses to paths based on this token.
-        let connection = Connection::new_session()?;
-        let sender_token = String::from(&connection.unique_name().replace(".", "_")[1..]);
-        println!("Connection::{:?}", sender_token);
-        Ok(ConnectionState {
-            connection,
-            sender_token,
-        })
-    }
-
-    /// Create a proxy to the main desktop portal object
-    pub fn desktop_proxy(&self) -> Proxy<&Connection> {
-        self.connection.with_proxy(
-            "org.freedesktop.portal.Desktop",
-            "/org/freedesktop/portal/desktop",
-            Duration::from_secs(20),
-        )
-    }
-}
-
-/// A single stream open in the capture session
-#[derive(Debug)]
-struct CaptureStream {
-    pipewire_node: u64,
-    // TODO: other parts of the stream
-}
-
-impl convert::From<PropMap> for CaptureStream {
-    fn from(results: PropMap) -> Self {
-        let streams = results.get("streams").unwrap();
-        println!("streams: {0:?}", streams);
-        for inner in streams.as_iter().unwrap() {
-            for inner_inner in inner.as_iter().unwrap() {
-                println!(
-                    "IIN: {0:?}",
-                    inner_inner.as_iter().unwrap().next().unwrap().as_u64()
-                );
-            }
-        }
-
-        CaptureStream { pipewire_node: 47 }
-    }
-}
-
-fn proxied_request<TResponse, RequestHandler, ResponseHandler>(
-    state: &ConnectionState,
-    make_request: RequestHandler,
-    mut on_response: ResponseHandler,
-) -> Result<TResponse, Box<dyn Error>>
-where
-    ResponseHandler: FnMut(OrgFreedesktopPortalRequestResponse) -> TResponse + Send + 'static,
-    RequestHandler: FnOnce(&str) -> Result<(), Box<dyn Error>>,
-    TResponse: Send + Sync + 'static,
-{
-    // Portal requests return their results via messages to a `Response` object.
-    // To protect against race conditions we open a proxy to the expected
-    // response object before making the call.
-    let (s, r) = std::sync::mpsc::channel();
-    let request_id = format!("screencap{0}", rand::random::<usize>());
-    let resp_path = Path::new(format!(
-        "/org/freedesktop/portal/desktop/request/{0}/{1}",
-        state.sender_token, request_id
-    ))?;
-    println!("@response path: {:?}", resp_path);
-    let resp_proxy = state.connection.with_proxy(
-        "org.freedesktop.portal.Desktop",
-        resp_path,
-        Duration::from_secs(20),
-    );
-    let id = resp_proxy.match_signal(
-        move |a: OrgFreedesktopPortalRequestResponse, _: &Connection, _: &Message| {
-            let res = on_response(a);
-            s.send(res).is_ok()
-        },
-    )?;
-
-    make_request(&request_id)?;
-
-    // Pump the event loop until we receive our expected result
-    loop {
-        if let Ok(data) = r.try_recv() {
-            resp_proxy.match_stop(id, true)?;
-            return Ok(data);
-        } else {
-            state.connection.process(Duration::from_millis(100))?;
-        }
-    }
-}
+mod portal;
 
 /// # Run the Test Application
 ///
 /// We have two main moving parts here. First we make D-Bus calls to obtain a
-/// ScreenCast session and start it. Once we have done that we connect to 
+/// ScreenCast session and start it. Once we have done that we connect to
 /// the raw video using Pipewire.
 fn main() -> Result<(), Box<dyn Error>> {
+    // - - - - - - - - - - - - - - PORTAL - - - - - - - - - - - - - -
+    let mut screen_cast = ScreenCast::new()?;
+    println!("Source types: {0:?}", screen_cast.source_types());
+    screen_cast.set_source_types(portal::SourceType::WINDOW);
+    let screen_cast = screen_cast.start(None)?;
 
-    // - - - - - - - - - - - - - - PORTAL - - - - - - - - - - - - - - 
-
-    let state = ConnectionState::open_new()?;
-
-    // Create a proxy pointing to the main desktop portal. We can then call
-    // ScreenCast interface method on this.
-    let desktop_proxy = state.desktop_proxy();
-
-    // Grab the supported cursor and source types. These are packed bitfields
-    println!(
-        "cursor modes: {:?}",
-        desktop_proxy.available_cursor_modes()?
-    );
-    println!(
-        "source types: {:?}",
-        desktop_proxy.available_source_types()?
-    );
-
-    let session = proxied_request(
-        &state,
-        |request_id| {
-            // Make the initail call to open the session.
-            let mut session_args = HashMap::<String, Variant<Box<dyn RefArg>>>::new();
-            session_args.insert(
-                "handle_token".into(),
-                Variant(Box::new(String::from(request_id))),
-            );
-            session_args.insert(
-                "session_handle_token".into(),
-                Variant(Box::new(String::from(request_id))),
-            );
-            println!("requesting session: {0:#?}", session_args);
-            state.desktop_proxy().create_session(session_args)?;
-            Ok(())
-        },
-        |a| {
-            a.results
-                .get("session_handle")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_owned()
-        },
-    )?;
-    println!("@GOT session: {0}", session);
-
-    proxied_request(
-        &state,
-        |request_id| {
-            let session = dbus::Path::from(&session);
-            let mut select_args = HashMap::<String, Variant<Box<dyn RefArg>>>::new();
-            select_args.insert(
-                "handle_token".into(),
-                Variant(Box::new(String::from(request_id))),
-            );
-            select_args.insert(
-                "types".into(),
-                Variant(Box::new(desktop_proxy.available_source_types()?)),
-            );
-            desktop_proxy.select_sources(session, select_args)?;
-            Ok(())
-        },
-        |_| (),
-    )?;
-
-    let stream = proxied_request(
-        &state,
-        |request_id| {
-            let session = dbus::Path::from(&session);
-            let mut select_args = HashMap::<String, Variant<Box<dyn RefArg>>>::new();
-            select_args.insert(
-                "handle_token".into(),
-                Variant(Box::new(String::from(request_id))),
-            );
-            desktop_proxy.start(session, "", select_args)?;
-            Ok(())
-        },
-        |response| CaptureStream::from(response.results),
-    )?;
-    println!("Stream: {0:?}", stream);
-
-    let pipe_fd =
-        desktop_proxy.open_pipe_wire_remote(dbus::Path::from(&session), HashMap::new())?;
-    println!("Pipewire FD: {0:?}", pipe_fd);
-
-    // - - - - - - - - - - - - - - PIPEWIRE - - - - - - - - - - - - - - 
+    // - - - - - - - - - - - - - - PIPEWIRE - - - - - - - - - - - - - -
 
     pipewire::init();
     let pw_loop = pipewire::MainLoop::new()?;
@@ -215,7 +30,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     unsafe {
         let pw_core = pipewire_sys::pw_context_connect_fd(
             pw_context.as_ptr(),
-            pipe_fd.into_fd(),
+            screen_cast.pipewire_fd(),
             ptr::null_mut(),
             0,
         );
---
+++ src/portal.rs
@@ -0,0 +1,305 @@
+//! # XDG ScreenCast Portal utilities
+//!
+//! This module defines an interface for interacting with the ScreenCast portal.
+//!
+//! The general interaction pattern with the `ScreenCast` portal is to open a
+//! session, set which source types are of interest, and call `start()`.
+
+use bitflags::bitflags;
+use dbus::{
+    arg::{OwnedFd, RefArg, Variant},
+    blocking::{Connection, Proxy},
+    channel::Token,
+    Message, Path,
+};
+use generated::{OrgFreedesktopPortalRequestResponse, OrgFreedesktopPortalScreenCast};
+use std::{
+    collections::HashMap,
+    os::unix::prelude::RawFd,
+    sync::mpsc::{self, Receiver},
+    time::Duration,
+};
+
+mod generated;
+
+// - - - - - - - - - - - - - - -  Public Interface - - - - - - - - - - - - - -
+
+#[derive(Debug)]
+pub enum PortalError {
+    Generic(String),
+    DBus(dbus::Error),
+}
+
+impl std::convert::From<String> for PortalError {
+    fn from(error_string: String) -> Self {
+        PortalError::Generic(error_string)
+    }
+}
+
+impl std::convert::From<dbus::Error> for PortalError {
+    fn from(err: dbus::Error) -> Self {
+        PortalError::DBus(err)
+    }
+}
+
+impl std::fmt::Display for PortalError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "D-Bus Portal error: {0:?}", self)
+    }
+}
+
+impl std::error::Error for PortalError {}
+
+pub struct ScreenCast {
+    state: ConnectionState,
+    session: String,
+    source_types: Option<SourceType>,
+}
+
+impl ScreenCast {
+    /// Create a new ScreenCast Session
+    ///
+    /// Connects to D-Bus and initaialises a ScreenCast object.
+    pub fn new() -> Result<Self, PortalError> {
+        let state = ConnectionState::open_new()?;
+
+        let session = {
+            let request = Request::with_handler(&state, |a| {
+                a.results
+                    .get("session_handle")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_owned()
+            })?;
+            // Make the initail call to open the session.
+            let mut session_args = HashMap::<String, Variant<Box<dyn RefArg>>>::new();
+            session_args.insert(
+                "handle_token".into(),
+                Variant(Box::new(String::from(&request.handle))),
+            );
+            session_args.insert(
+                "session_handle_token".into(),
+                Variant(Box::new(String::from(&request.handle))),
+            );
+            state.desktop_proxy().create_session(session_args)?;
+            request.wait_response()?
+        };
+
+        Ok(ScreenCast {
+            state,
+            session,
+            source_types: None,
+        })
+    }
+
+    /// Get the supported source types for this connection
+    pub fn source_types(&self) -> Result<SourceType, PortalError> {
+        let types = self.state.desktop_proxy().available_source_types()?;
+        Ok(SourceType::from_bits_truncate(types))
+    }
+
+    /// Set the source types to capture. This should be a subset of
+    /// those from `source_types()`.
+    pub fn set_source_types(&mut self, types: SourceType) {
+        self.source_types = Some(types);
+    }
+
+    /// Try to start the screen cast.
+    pub fn start(self, parent_window: Option<&str>) -> Result<ActiveScreenCast, PortalError> {
+        let desktop_proxy = self.state.desktop_proxy();
+
+        {
+            let request = Request::new(&self.state)?;
+            let session = dbus::Path::from(&self.session);
+            let mut select_args = HashMap::<String, Variant<Box<dyn RefArg>>>::new();
+            select_args.insert(
+                "handle_token".into(),
+                Variant(Box::new(String::from(&request.handle))),
+            );
+            select_args.insert(
+                "types".into(),
+                Variant(Box::new(match self.source_types {
+                    Some(types) => types.bits(),
+                    None => desktop_proxy.available_source_types()?,
+                })),
+            );
+            desktop_proxy.select_sources(session, select_args)?;
+            request.wait_response()?;
+        }
+
+        let streams = {
+            let request = Request::new(&self.state)?;
+            let session = dbus::Path::from(&self.session);
+            let mut select_args = HashMap::<String, Variant<Box<dyn RefArg>>>::new();
+            select_args.insert(
+                "handle_token".into(),
+                Variant(Box::new(String::from(&request.handle))),
+            );
+            desktop_proxy.start(session, parent_window.unwrap_or(""), select_args)?;
+            request.wait_response()?;
+
+            // FIXME: deserialise streams properly
+            Vec::new()
+        };
+
+        let pipewire_fd =
+            desktop_proxy.open_pipe_wire_remote(dbus::Path::from(&self.session), HashMap::new())?;
+
+        Ok(ActiveScreenCast {
+            state: self.state,
+            pipewire_fd,
+            streams,
+        })
+    }
+}
+
+/// An active ScreenCast session. This holds a file descriptor for connecting
+/// to PipeWire along with metadata for the active streams.
+pub struct ActiveScreenCast {
+    state: ConnectionState,
+    pipewire_fd: OwnedFd,
+    streams: Vec<ScreenCastStream>,
+}
+
+impl ActiveScreenCast {
+    /// Get the fille descriptor for the PipeWire session.
+    pub fn pipewire_fd(&self) -> RawFd {
+        self.pipewire_fd.clone().into_fd()
+    }
+
+    /// Get the streams active in this ScreenCast.
+    pub fn streams(&self) -> impl Iterator<Item = &ScreenCastStream> {
+        self.streams.iter()
+    }
+
+    // TODO: Close API + drop Impl
+}
+
+pub struct ScreenCastStream {}
+
+bitflags! {
+    pub struct SourceType : u32  {
+        const MONITOR = 0b00001;
+        const WINDOW = 0b00010;
+    }
+}
+
+// - - - - - - - - - - - - - -  Private Implementation - - - - - - - - - - - -
+
+/// D-Bus connection state. Used to access the Desktop portal
+/// and open our screencast.
+struct ConnectionState {
+    connection: Connection,
+    sender_token: String,
+}
+
+impl ConnectionState {
+    /// Open a new D-Bus connection to use for all our requests
+    pub fn open_new() -> Result<Self, dbus::Error> {
+        // Create a new session and work out our session's sender token. Portal
+        // requests will send responses to paths based on this token.
+        let connection = Connection::new_session()?;
+        let sender_token = String::from(&connection.unique_name().replace(".", "_")[1..]);
+        Ok(ConnectionState {
+            connection,
+            sender_token,
+        })
+    }
+
+    /// Create a proxy to the main desktop portal object
+    pub fn desktop_proxy(&self) -> Proxy<&Connection> {
+        self.connection.with_proxy(
+            "org.freedesktop.portal.Desktop",
+            "/org/freedesktop/portal/desktop",
+            Duration::from_secs(20),
+        )
+    }
+}
+
+/// A request object. Portal requests are used to wait for responses to ongoing
+/// portal operations.
+struct Request<'a, Response> {
+    /// A proxy connected to this reuqest object on the bus.
+    proxy: Proxy<'a, &'a Connection>,
+    /// The handle for this request.
+    handle: String,
+    /// The channel reciever that we can read responses from.
+    response: Receiver<Response>,
+    /// The match token to remove our D-Bus matcher.
+    match_token: Token,
+}
+
+impl<'a> Request<'a, ()> {
+    /// Create a new request object with the given connection. This generates
+    /// a random token for the handle.
+    pub fn new(state: &'a ConnectionState) -> Result<Self, PortalError> {
+        Self::with_handler(state, |_| {})
+    }
+}
+
+impl<'a, Response> Request<'a, Response> {
+    /// Create a new request object with the given connection and handler. This
+    /// generates a random token for the handle. The results of the handler can
+    /// be retrieved by calling `wait_result()`.
+    pub fn with_handler<ResponseHandler>(
+        state: &'a ConnectionState,
+        mut on_response: ResponseHandler,
+    ) -> Result<Self, PortalError>
+    where
+        ResponseHandler: FnMut(OrgFreedesktopPortalRequestResponse) -> Response + Send + 'static,
+        Response: Send + 'static,
+    {
+        let handle = format!("screencap{0}", rand::random::<usize>());
+        let resp_path = Path::new(format!(
+            "/org/freedesktop/portal/desktop/request/{0}/{1}",
+            state.sender_token, handle
+        ))?;
+        let proxy = state.connection.with_proxy(
+            "org.freedesktop.portal.Desktop",
+            resp_path,
+            Duration::from_secs(20),
+        );
+        let (sender, response) = mpsc::channel();
+        let match_token = proxy.match_signal(
+            move |a: OrgFreedesktopPortalRequestResponse, _: &Connection, _: &Message| {
+                // FIXME: handle error responses here somehow? Currently it is
+                //        just up to the `on_response` to deal with it.
+                let res = on_response(a);
+                sender.send(res).is_ok()
+            },
+        )?;
+        Ok(Request {
+            proxy,
+            handle,
+            response,
+            match_token,
+        })
+    }
+
+    pub fn wait_response(&self) -> Result<Response, PortalError> {
+        // Pump the event loop until we receive our expected result
+        loop {
+            if let Ok(data) = self.response.try_recv() {
+                return Ok(data);
+            } else {
+                self.proxy.connection.process(Duration::from_millis(100))?;
+            }
+        }
+    }
+}
+
+impl<'a, T> std::ops::Drop for Request<'a, T> {
+    fn drop(&mut self) {
+        let _ = self.proxy.match_stop(self.match_token, true);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    pub fn it_works() {
+        assert!(false)
+    }
+}
